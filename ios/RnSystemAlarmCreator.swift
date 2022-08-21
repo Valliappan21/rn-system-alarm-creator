6f1d2f58c3f9a59590b1382df0b0deb77872c054
Valliappan Periannan <valliappan21@gmail.com>
2022-08-21 07:19:05 +0530

    Delete ios directory

--- ios/RnSystemAlarmCreator.swift
+++
@@ -1,8 +0,0 @@
-@objc(RnSystemAlarmCreator)
-class RnSystemAlarmCreator: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
-}
