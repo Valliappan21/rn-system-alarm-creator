94209c38b44ead104bcf0e138f8201e88735cef4
Valliappan <valliappan21@gmail.com>
2022-08-29 23:49:07 +0530

    customization

--- android/src/main/java/com/rnsystemalarmcreator/RnSystemAlarmCreatorModule.kt
+++ android/src/main/java/com/rnsystemalarmcreator/RnSystemAlarmCreatorModule.kt
@@ -2,10 +2,10 @@ package com.rnsystemalarmcreator
 
 import android.content.Intent
 import android.provider.AlarmClock
-import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.*
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class RnSystemAlarmCreatorModule(reactContext: ReactApplicationContext) :
     ReactContextBaseJavaModule(reactContext) {
@@ -15,8 +15,20 @@ class RnSystemAlarmCreatorModule(reactContext: ReactApplicationContext) :
     }
 
     @ReactMethod
-    fun setAlarm(hours: Int, mins: Int, name: String) {
+    fun setAlarm(data:ReadableMap) {
+        val hours =data.getInt("hours")
+        val mins = data.getInt("mins");
+        val name = data.getString("name");
+        val alarmDays = ArrayList<Int>()
+        if(data.hasKey("days")){
+            val days= data.getArray("days")!!.toArrayList()
+            days.forEach { it ->
+                alarmDays.add(it as Int)
+            }
+        }
         val i = Intent(AlarmClock.ACTION_SET_ALARM)
+//        val alarmDays = ArrayList<Int>()
+        alarmDays.add(Calendar.SATURDAY)
         i.putExtra("android.intent.extra.alarm.HOUR", hours)
         i.putExtra("android.intent.extra.alarm.MINUTES", mins)
         i.putExtra("android.intent.extra.alarm.MESSAGE", name)
